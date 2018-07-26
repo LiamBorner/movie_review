44b4c47c83dd19994c0c106d44182437cbe26840
Liam Borner <liamborner@gmail.com>
2018-07-26 20:06:26 +0100

    reviews added in

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_25_212922) do
+ActiveRecord::Schema.define(version: 2018_07_26_185251) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -43,6 +43,14 @@ ActiveRecord::Schema.define(version: 2018_07_25_212922) do
     t.datetime "updated_at", null: false
     t.integer "user_id"
     t.string "image"
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.integer "rating"
+    t.text "comment"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
