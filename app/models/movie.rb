fb0d60ebad5939264b4cfb19edcd71015a8bccb4
Liam Borner <liamborner@gmail.com>
2018-07-26 20:13:11 +0100

    merging movies and review routes

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,5 +1,6 @@
 class Movie < ApplicationRecord
   belongs_to :user, :optional => true
   has_one_attached :image
+  has_many :reviews
   # validates_attachement_content_type :image, :content_type => /\Aimage\/.*\Z/
 end
